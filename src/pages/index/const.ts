9ab32e097ffddfe0955ebd55e7bc193bb1b60825
daisy219 <daisyzhao219@outlook.com>
2020-09-18 16:02:29 +0800

    '首页'

--- src/pages/index/const.ts
+++ src/pages/index/const.ts
@@ -1,31 +1,64 @@
 export const firstModuleArr = [
   {
-    icon: 'menu1-1',
+    icon: 'menu1_1',
+    title: '健康管理',
+    path: '',
+  },
+  {
+    icon: 'menu1_2',
+    title: '我的福利',
+    path: '',
+  },
+  {
+    icon: 'menu1_3',
+    title: '服务指南',
+    path: '',
+  },
+  {
+    icon: 'menu1_4',
+    title: '入离职办理',
+    path: '',
+  },
+  {
+    icon: 'menu1_5',
+    title: '薪资试算',
+    path: '',
+  },
+  {
+    icon: 'menu1_6',
+    title: '专属客服',
+    path: '',
+  },
+];
+
+export const secondModuleArr = [
+  {
+    icon: 'menu2_1',
     title: '基本信息',
     path: '',
   },
   {
-    icon: 'menu1-2',
+    icon: 'menu2_2',
     title: '社保查询',
     path: '',
   },
   {
-    icon: 'menu1-3',
+    icon: 'menu2_3',
     title: '薪资查询',
     path: '',
   },
   {
-    icon: 'menu1-4',
+    icon: 'menu2_4',
     title: '公积金查询',
     path: '',
   },
   {
-    icon: 'menu1-5',
+    icon: 'menu2_5',
     title: '报销查询',
     path: '',
   },
   {
-    icon: 'menu1-6',
+    icon: 'menu2_6',
     title: '开具证明',
     path: '',
   },
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="home-page-index">
+  <view class="home-page-index common-page">
     <view class="clearfix">
       <text class="logo-text">{{ logo }}</text>
       <view class="filter-text">简体中文<van-icon name="arrow-down"/></view>
@@ -9,24 +9,69 @@
         <img v-lazy="image" class="swipe-image" />
       </van-swipe-item>
     </van-swipe>
-
-    <view class="">
-      <view v-for="item in firstModuleArr" :key="item.icon">
-        <!-- <img :src="require('../../assets/image/home/menu1-1.png')"/> -->
-        <van-image width="30px" height="30px" fit="cover" :src="require('../../assets/image/home/menu1-1.png')"/>
-      </view>
+    <van-row class="second-module">
+      <van-col span="8" v-for="item in secondModuleArr" :key="item.icon" class="second-item">
+        <van-image width="50" height="50" fit="contain" :src="menu[item.icon]" />
+        <view class="item-title">{{ item.title }}</view>
+      </van-col>
+    </van-row>
+    <view class="bg-wrap">
+      <van-image width="100%" height="150px" fit="fill" :src="bgImg" />
     </view>
+
+    <van-row class="first-module" type="flex" justify="space-between">
+      <van-col span="7" v-for="(item, index) in firstModuleArr.slice(0, 3)" :key="item.icon" class="first-item">
+        <van-image width="34" height="34" fit="fill" :src="menu[item.icon]" />
+        <view class="item-title">{{ item.title }}</view>
+      </van-col>
+    </van-row>
+    <van-row class="first-module" type="flex" justify="space-between">
+      <van-col span="7" v-for="(item, index) in firstModuleArr.slice(3, 6)" :key="item.icon" class="first-item">
+        <van-image width="34" height="34" fit="fill" :src="menu[item.icon]" />
+        <view class="item-title">{{ item.title }}</view>
+      </van-col>
+    </van-row>
   </view>
 </template>
 
 <script>
 import './index.less';
-import { firstModuleArr } from './const';
+import { firstModuleArr, secondModuleArr } from './const';
+import menu1_1 from '../../assets/image/home/menu1-1.png';
+import menu1_2 from '../../assets/image/home/menu1-2.png';
+import menu1_3 from '../../assets/image/home/menu1-3.png';
+import menu1_4 from '../../assets/image/home/menu1-4.png';
+import menu1_5 from '../../assets/image/home/menu1-5.png';
+import menu1_6 from '../../assets/image/home/menu1-6.png';
+import menu2_1 from '../../assets/image/home/menu2-1.png';
+import menu2_2 from '../../assets/image/home/menu2-2.png';
+import menu2_3 from '../../assets/image/home/menu2-3.png';
+import menu2_4 from '../../assets/image/home/menu2-4.png';
+import menu2_5 from '../../assets/image/home/menu2-5.png';
+import menu2_6 from '../../assets/image/home/menu2-6.png';
+import bgImg from '../../assets/image/home/bg.jpg';
+// const test = require('../../assets/image/home/menu1-1.png');
 
 export default {
   data () {
     return {
+      menu: {
+        menu1_1: menu1_1,
+        menu1_2: menu1_2,
+        menu1_3: menu1_3,
+        menu1_4: menu1_4,
+        menu1_5: menu1_5,
+        menu1_6: menu1_6,
+        menu2_1: menu2_1,
+        menu2_2: menu2_2,
+        menu2_3: menu2_3,
+        menu2_4: menu2_4,
+        menu2_5: menu2_5,
+        menu2_6: menu2_6,
+      },
+      bgImg: bgImg,
       firstModuleArr: firstModuleArr,
+      secondModuleArr: secondModuleArr,
       logo: 'ciic',
       images: [
         'https://img.yzcdn.cn/vant/apple-1.jpg',
